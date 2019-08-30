b3a54b9cb952e2881830c2ae6bb60a59fd39fd1e
Jagnoor Singh <jagnoor.singh@cerner.net>
2019-08-30 12:56:43 -0400

    add weekday

--- Paystation.java
+++ Paystation.java
@@ -9,15 +9,19 @@ public class Paystation
     private int nickles = 5;
     private int dimes = 10;
     private int quarters = 25;
+    private boolean weekday= true;
 
 
     
     public void Start(){
         Scanner sc = new Scanner(System.in);
-        
+
         while(true){
         System.out.println("How many pennies are you going to inste");
         }
+
+        System.out.println("Is it a weekday? Enter true or false: \n");
+        weekday= sc.nextline();
     }
     
     
@@ -29,12 +33,18 @@ public class Paystation
 
     public int CalculateTime(int amount) {
         int time = 0;
-        if (amount < 60) {
+        if (weekday == false) {
+            if (amount < 60) {
+                time = (amount * 2) / 5;
+            } else if (amount <= 120) {
+                time = (amount - 150) * (3 / 10) + 60;
+            } else if (amount > 120) {
+                time = (amount - 350) / 5 + 120;
+            }
+        }
+
+        else {
             time = (amount * 2) / 5;
-        } else if (amount <= 120) {
-            time = (amount - 150) * (3 / 10) + 60;
-        } else if (amount > 120) {
-            time = (amount - 350) / 5 + 120;
         }
         return time;
     }
